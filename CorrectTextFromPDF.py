8e7d9d2510c3e7073d1f82d45a8f5d8261a14006
Mikael Folkesson <mikael.folkesson@gmail.com>
2023-04-10 15:03:09 +0200

    Text sent to OpenAI in max 2k token chunks

--- CorrectTextFromPDF.py
+++ CorrectTextFromPDF.py
@@ -7,12 +7,21 @@ from PyPDF2 import PdfReader
 import os
 import openai
 import re
+import textwrap
 
-# creating a pdf reader object
 PDFpath='./'
 openai.api_key = os.getenv("OPENAI_API_KEY")
+max_width = 7777
+
+def process_text(text, max_width):
+    # Wrap text into paragraphs of a given width without breaking lines within words
+    wrapped_text = textwrap.fill(text, width=max_width)
+    # Split wrapped text into chunks of a given size
+    chunks = textwrap.wrap(wrapped_text, width=max_width)
+    return chunks
 
 def proofread_page(text):
+    #Send to OpenAI for proof reading and return result
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=("Korrigera stavfel på svenska utan att lägga till någonting varken före eller efter. " 
@@ -48,13 +57,15 @@ for filename in os.listdir(PDFpath):
                 replacement = "" # replaces the match with an empty string
                 extracted_text = re.sub (pattern, replacement, extracted_text) # performs the substitution
 
-                #Process the text in chunks of 7777 characters, roughly 2000 tokens, which is the OpenAI API limit
-                
-            #    for i, page_text in enumerate(re.findall(r"\S.{0,7777}\S(?=\s|$)", extracted_text)):
-                    # Split text into chunks of roughly 2000 tokens to fit OpenAI API limit
-                corrected_page_text = proofread_page(extracted_text)
-                corrected_text += f"Sida {i+1}\n\n{corrected_page_text}\n\n"
-                
+                #Process the text in chunks of 7777 characters, roughly 2000 tokens, which is the OpenAI API limit              
+                chunks = process_text(extracted_text, max_width)
+                for chunk in chunks:
+                    corrected_page_text = proofread_page(chunk)
+                    if (chunk==chunks[0]):
+                        corrected_text += f"Sida {i+1}\n\n{corrected_page_text}\n\n"
+                    else:
+                        corrected_text += f"{corrected_page_text}\n\n"
+                    
             #    print("Corrected text: "+response.choices[0].text)
             #    corrected_text += 'Sida ' + str(i+1) + "\n\n" + response.choices[0].text + "\n\n"
                 i+=1
